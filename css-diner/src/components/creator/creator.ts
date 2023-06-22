c88ca0ab954394146841b35a9c58624b758d52cc
l1senochek <lisenochek96@gmail.com>
2023-06-22 14:18:44 +0300

    feat: update files project

---
+++ css-diner/src/components/creator/creator.ts
@@ -0,0 +1,86 @@
+export type Callback<T> = (data?: T) => void;
+
+export interface ElementParam {
+  tag: string,
+  classNames: Array<string>,
+  textContent: string,
+  callback: Callback<Event>,
+}
+
+export default class CreatorElement {
+  elem: HTMLElement | null;
+
+  constructor(param: ElementParam) {
+    this.elem = null;
+    this.createElem(param);
+  }
+
+  // /**
+  //  * @returns {HTMLElement}
+  //  */
+  // getElement() {
+  //   return this.elem;
+  // }
+
+  // /**
+  //  * @param {HTMLElement | ElementCreator} element
+  //  */
+  // addInnerElement(element) {
+  //   if (element instanceof ElementCreator) {
+  //     this.elem.append(element.getElement());
+  //   } else {
+  //     this.elem.append(element);
+  //   }
+  // }
+
+  /**
+   * @param {Elementparam} param
+   */
+
+
+
+  createElem(param: ElementParam) {
+    this.elem = document.createElement(param.tag);
+    if (this.elem instanceof HTMLElement) {
+      // this.setCssClasses(param.classNames);
+      param.classNames.map((cssClass) => {
+
+        this.elem.classList.add(cssClass);
+      });
+      
+
+      // param.classNames.forEach(cssClass => {
+      //   this.elem.classList.add(cssClass);
+      // });
+
+
+      this.elem.textContent = param.textContent;
+      // this.setCallback(param.callback);
+    }
+    
+    
+  }
+
+  /**
+   * @param {Array<string>} cssClasses
+   */
+  setCssClasses(cssClasses = []) {
+    cssClasses.map((cssClass) => this.elem.classList.add(cssClass));
+  }
+
+  /**
+   * @param {string} text
+   */
+  setTextContent(text = '') {
+    this.elem.textContent = text;
+  }
+
+  /**
+   * @param {function} callback
+   */
+  setCallback(callback) {
+    if (typeof callback === 'function') {
+      this.elem.addEventListener('click', (event) => callback(event));
+    }
+  }
+}
