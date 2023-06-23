e7b428e2da48b71dd6315dbc32ba672bea117758
l1senochek <lisenochek96@gmail.com>
2023-06-23 15:02:41 +0300

    feat: update css file

--- css-diner/src/index.ts
+++ css-diner/src/index.ts
@@ -1,3 +1,5 @@
 import { App } from "./components/app/app";
+import './css/index.css';
+
 
 const app = new App();
--- css-diner/webpack.config.js
+++ css-diner/webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
